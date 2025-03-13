6d7dcf2c37835457454c374012f41529ac51cf48
Rohan Shergill <rohan.shergill1875@gmail.com>
2025-03-13 15:37:05 +0000

    Initial Commit

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Coming Soon
+        </p>
+        
+        <b>- Rohan Shergill</b>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
